8545028aae162816de2387b35dc6a04cdd5a5908
napoleonrafael <cernanapoleon@outlook.com>
2020-08-09 11:02:05 -0700

    Create BackEnd Skeleton

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3030;
 
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
-const moviesRouter = require('./routes/movies');
+const zipcodeRouter = require('./routes/zipcode');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true })); // <---- ADDED
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/movies', moviesRouter);
+app.use('/zipcode', zipcodeRouter);
 
 app.listen(PORT, (err) =>
   console.log(`${err ? err : `running on PORT ${PORT}`}`),
